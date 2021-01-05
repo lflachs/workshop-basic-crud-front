2802d1b01b538ef05f1d922578ca18793e595d4a
Leonard <leonard.flachs@gmail.com>
2021-01-05 18:13:54 +0100

    :tada: first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
 import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [todolistTitle, setTodolistTitle] = useState('');
+	const [updateTodolistTitle, setupdateTodolistTitle] = useState('');
+	const [updateTodolistId, setupdateTodolistId] = useState('');
+	const [todolists, setTodolists] = useState([]);
+	useEffect(() => {
+		fetch('http://localhost:8000/todolist')
+			.then((resp) => resp.json())
+			.then((todolists) => setTodolists(todolists))
+			.catch((err) => console.log(err));
+	}, []);
+	const handleDelete = (todolistId) => {
+		fetch(`http://localhost:8000/todolist/${todolistId}`, {
+			method: 'DELETE',
+		})
+			.then((resp) => resp.json())
+			.then((deletedTodolist) =>
+				setTodolists(todolists.filter((todo) => todo.id !== deletedTodolist.id))
+			)
+			.catch((err) => console.log(err));
+	};
+	const handleUpdate = (event) => {
+		event.preventDefault();
+		fetch(`http://localhost:8000/todolist/${updateTodolistId}`, {
+			method: 'PUT',
+			headers: {
+				Accept: 'application/json',
+				'content-Type': 'application/json',
+			},
+			body: JSON.stringify({ title: updateTodolistTitle }),
+		})
+			.then((resp) => resp.json())
+			.then((updatedTodolist) => {
+				const oldTodo = todolists.filter(
+					(todo) => todo.id !== updatedTodolist.id
+				);
+
+				console.log('todos', oldTodo);
+				setTodolists([...oldTodo, updatedTodolist]);
+			})
+			.catch((err) => console.log(err));
+	};
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		setTodolistTitle('');
+		fetch('http://localhost:8000/todolist', {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'content-Type': 'application/json',
+			},
+			body: JSON.stringify({ title: todolistTitle }),
+		})
+			.then((resp) => resp.json())
+			.then((newTodo) =>
+				setTodolists((todolist) => {
+					return [...todolist, newTodo];
+				})
+			)
+			.catch((err) => console.log(err));
+	};
+	return (
+		<div className='App'>
+			<ul>
+				{todolists.map((todolist) => {
+					return (
+						<li key={todolist.id} onClick={() => handleDelete(todolist.id)}>
+							{todolist.id} = {todolist.title}
+						</li>
+					);
+				})}
+			</ul>
+			<h1>Create a todolist</h1>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor='todolist-title'>Title:</label>
+				<input
+					type='text'
+					value={todolistTitle}
+					onChange={(event) => setTodolistTitle(event.target.value)}
+				/>
+				<input type='submit' value='send'></input>
+			</form>
+			<h1>Update a todolist</h1>
+			<form onSubmit={handleUpdate}>
+				<label htmlFor='todolist-title'>Title:</label>
+				<input
+					type='text'
+					value={updateTodolistTitle}
+					onChange={(event) => setupdateTodolistTitle(event.target.value)}
+				/>
+				<br />
+				<label htmlFor='todolist-title'>id:</label>
+				<input
+					type='number'
+					value={updateTodolistId}
+					onChange={(event) => setupdateTodolistId(event.target.value)}
+				/>
+				<input type='submit' value='send'></input>
+			</form>
+		</div>
+	);
 }
 
 export default App;
